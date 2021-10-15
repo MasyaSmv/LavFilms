042e267642e43894c6f70da3681e0b286ff6eda1
MasyaSm <kuvalda-max@mail.ru>
2021-10-15 10:49:21 +0300

    readme

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('auth.lcab.home');
+    }
+
+    public function favorites()
+    {
+        return view('auth.favorites.index');
     }
 }
